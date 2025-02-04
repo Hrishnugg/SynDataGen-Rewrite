461face61505ff8ddea8d7abbb7b7c2e46814a45
Hrishnugg <hrishihari51@gmail.com>
2025-02-04 11:15:27 -0500

    Changed head files

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
->>>>>>> 131d95b2 (first commit)
 } 
